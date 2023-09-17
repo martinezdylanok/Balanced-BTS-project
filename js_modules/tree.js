7224b69db66cc14601796d414699c6f36df468ac
Lpik97 <dm97fx@gmail.com>
2023-09-17 09:54:21 +0200

    feat(tree class modified)
    The tree class now has a constant variable called sortedArray which uses the later called sortAndRemoveDuplicates function. Then, for the root, it also uses a later called function called buildTree, using the sortedArray as one of the parameters. The sortAndRemoveDuplicates function will take an array as argument, sort it in ascendant order using the sort method, then using that sorted array to filter it, creating a new array that contains only the unique values.

--- js_modules/tree.js
+++ js_modules/tree.js
@@ -1,5 +1,15 @@
+import node from "./node";
+
 export default class tree {
-    constructor () {
-        this.root = null;
+    constructor (array) {
+        const sortedArray = sortAndRemoveDuplicates(array);
+        this.root = buildTree(sortedArray, 0, sortedArray.lengt - 1);
+    }
+
+    sortAndRemoveDuplicates(array) {
+
+        const sortedArray = array.sort((a, b) => a - b);
+
+        return sortedArray.filter((value, index, self) => self.indexOf(value) === index);
     }
 }
