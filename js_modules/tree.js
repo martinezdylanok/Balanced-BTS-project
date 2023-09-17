226ea44cfd78bd19b28aa05a2d475c51a2c16918
Lpik97 <dm97fx@gmail.com>
2023-09-17 09:58:41 +0200

    feat(buildtree property)
    The buildtree function will take an array, a start and a ending as parameters. The first conditional will be the base case, checking if the start is greater than the ending, returing null. Then a mid and root variables are created, the first one containing the sum of start and end then divided by two, with parseInt method applied to the result, and the root creating a new node object with the array at the mid index. For both leftChild and rightChild uses the buildTree recursively, using the array, start and mid -1 for the left as parameters, and array, mid +1 and end for the right. Then the root element is returned.

--- js_modules/tree.js
+++ js_modules/tree.js
@@ -12,4 +12,19 @@ export default class tree {
 
         return sortedArray.filter((value, index, self) => self.indexOf(value) === index);
     }
+
+    buildTree(array, start, end) {
+
+        if (start > end) {
+            return null;
+        }
+
+        let mid = parseInt(start + end) / 2;
+
+        let root = new node(array[mid]);
+        root.leftChild = buildTree(array, start, mid - 1);
+        root.rightChild = buildTree(array, mid + 1, end);
+
+        return root;
+    }
 }
